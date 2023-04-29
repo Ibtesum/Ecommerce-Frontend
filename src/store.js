f93e2c3b3c95758ef12ea2711d12b8acc1a145da
Ibtesum Reza Aninda <aninda7152@gmail.com>
2023-04-30 04:36:40 +0600

    Shipping Screen Done

--- src/store.js
+++ src/store.js
@@ -13,6 +13,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from "./reducers/userReducers";
+import { json } from "react-router-dom";
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -32,8 +33,15 @@ const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
